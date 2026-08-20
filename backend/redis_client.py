65fceab85d20d48a8068efa26f1b4eb8f279a5bb
Seher Akyel <seherakyl06@gmail.com>
2026-08-20 15:06:41 +0300

    add Rate limiting

--- backend/redis_client.py
+++ backend/redis_client.py
@@ -1,6 +1,21 @@
+from fastapi import HTTPException
 import os
 import redis
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+
+
+def check_rate_limit(key: str, limit: int, window: int):
+    current = redis_client.get(key)
+    if current is None:
+        redis_client.setex(key, window, 1)
+    else:
+        current = int(current)
+        if current >= limit:
+            raise HTTPException(
+                status_code=429,
+                detail="Çok fazla deneme yaptınız. Lütfen biraz bekleyin."
+            )
+        redis_client.incr(key)
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi import Request
 from dependencies import get_current_user
-import models
-
-import crud
-import schemas
-import auth
+from redis_client import check_rate_limit
 from database import get_db
+import models,crud,schemas,auth
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -17,7 +15,9 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/login", response_model=schemas.Token)
-def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+def login(user: schemas.UserLogin, request: Request, db: Session = Depends(get_db)):
+    client_ip = request.client.host
+    check_rate_limit(f"ratelimit:login:{client_ip}", limit=5, window=60)
     db_user = crud.authenticate_user(db, user.email, user.password)
     if not db_user:
         raise HTTPException(status_code=401, detail="E-posta veya şifre hatalı")
